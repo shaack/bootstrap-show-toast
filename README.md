f982abdcb7003a2b212cc0cfb79a949f374d7ca3
shaack <shaack@gmail.com>
2022-08-26 15:49:17 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,64 @@
 # bootstrap-show-toast
+
+A Bootstrap 5 plugin for [Bootstrap Toasts](https://getbootstrap.com/docs/5.2/components/toasts/), to show them with pure JS, no HTML needed.
+
+## Try it
+
+- [Demo Page](https://shaack.com/projekte/bootstrap-show-toast/)
+
+## Installation
+
+- [npm package](https://www.npmjs.com/package/bootstrap-show-toast)
+
+```sh
+npm install bootstrap-show-toast
+```
+
+## Usage
+
+```html
+<script src="./node_modules/bootstrap-show-notification/src/bootstrap-show-toast.js"></script>
+<script>
+    // simple example
+    $("#button-show-simple").click(function () {
+        $.showNotification({body: "Hello Toast!"})
+    })
+    // type info and more complex body
+    $("#button-show-info").click(function () {
+        $.showNotification({
+            body: "<h3>For your Info</h3>This notification has a title and a body and more text than the previous one.", type: "info"
+        })
+    })
+    // type danger
+    $("#button-show-danger").click(function () {
+        $.showNotification({
+            body: "Danger!", type: "danger"
+        })
+    })
+    // type secondary and sticky
+    $("#button-show-sticky").click(function () {
+        $.showNotification({
+            body: "This notification will stay", type: "secondary", duration: 0
+        })
+    })
+</script>
+```
+
+## Props (defaults)
+
+```js
+this.props = {
+    body: "", // the text, shown
+    type: "primary", // the appearance
+    duration: 5500, // duration till auto-hide, set to `0` to disable auto-hide
+    maxWidth: "520px" // the notification maxWidth
+}
+```
+
+## Documentation
+
+- [Repository and documentation](https://github.com/shaack/bootstrap-show-toast)
+
+## More Bootstrap extensions
+
+Check out my [further Bootstrap extensions](https://github.com/shaack?tab=repositories&q=bootstrap&type=&language=&sort=stargazers)
---
+++ src/bootstrap-show-toast.js
@@ -0,0 +1,90 @@
+/**
+ * Author and copyright: Stefan Haack (https://shaack.com)
+ * Repository: https://github.com/shaack/bootstrap-show-notification
+ * License: MIT, see file 'LICENSE'
+ */
+;(function (bootstrap) {
+    "use strict"
+
+    function createElement(html) {
+        const template = document.createElement('template')
+        template.innerHTML = html.trim()
+        return template.content.firstChild
+    }
+
+    function Toast(props) {
+        // see https://getbootstrap.com/docs/5.2/components/toasts/
+        this.props = {
+            header: "", // the header text
+            headerSmall: "", // additional text in the header, aligns right
+            body: "", // the body text of the toast
+            closeButton: true, // show a close button
+            closeButtonLabel: "close", // the label of the close button
+            closeButtonClass: "", // set to "btn-close-white" for dark backgrounds
+            toastClass: "", // the appearance
+            animation: true, // apply a CSS fade transition to the toast
+            delay: 5000, //	delay in milliseconds before hiding the toast, delay of 0 means infinite
+            position: "top-0 end-0", // top right
+            direction: "prepend", // or "append", the stack direction
+            ariaLive: "assertive"
+        }
+        this.containerId = "bootstrap-show-toast-container-" + this.props.position.replace(" ", "_")
+        for (let prop in props) {
+            // noinspection JSUnfilteredForInLoop
+            this.props[prop] = props[prop]
+        }
+        const cssClass = ("toast " + this.props.toastClass).trim()
+        let toastHeader = ""
+        const showHeader = this.props.header || this.props.headerSmall
+        if (showHeader) {
+            toastHeader = `<div class="toast-header">
+                            <strong class="me-auto">${this.props.header}</strong>
+                            ${this.props.headerSmall ? `<small>${this.props.headerSmall}</small>` : ""}
+                            ${this.props.closeButton ? `<button type="button" class="btn-close ${this.props.closeButtonClass}" data-bs-dismiss="toast" aria-label="${this.props.closeButtonLabel}"></button>` : ""}
+                          </div>`
+        }
+        this.template =
+            `<div class="${cssClass}" role="alert" aria-live="${this.props.ariaLive}" aria-atomic="true">
+              ${toastHeader}
+              <div class="d-flex">
+                  <div class="toast-body">
+                    ${this.props.body}
+                  </div>
+                  ${(!showHeader && this.props.closeButton) ? `<button type="button" class="btn-close me-2 mx-auto ${this.props.closeButtonClass}" style="margin-top: 0.69rem" data-bs-dismiss="toast" aria-label="${this.props.closeButtonLabel}"></button>` : ""}
+              </div>
+            </div>`
+        this.container = document.getElementById(this.containerId)
+        if (!this.container) {
+            this.container = document.createElement("div")
+            this.container.id = this.containerId
+            this.container.setAttribute("class", "toast-container position-fixed p-3 " + this.props.position)
+            document.body.appendChild(this.container)
+        }
+        this.element = createElement(this.template)
+        this.toast = this.showToast(this.element)
+    }
+
+    Toast.prototype.showToast = function (toastElement) {
+        if (this.props.direction === "prepend") {
+            this.container.prepend(toastElement)
+        } else {
+            this.container.append(toastElement)
+        }
+        toastElement.classList.add("show")
+        console.log(this.props.animation, this.props.delay > 0, this.props.delay)
+        /*
+        this.toast = new bootstrap.Toast(toastElement, {
+            animation: this.props.animation,
+            autohide: this.props.delay > 0,
+            delay: this.props.delay
+
+        })
+        */
+        return toastElement
+    }
+
+    bootstrap.showToast = function (props) {
+        return new Toast(props)
+    }
+
+}(bootstrap))
